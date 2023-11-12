ca5eedc6a3ac4a3107f9426326980954bf4f95e4
Rūta <gec.ruta@gmail.com>
2023-11-12 15:01:29 +0200

    Slight change in room navigation system

---
+++ Pages/RoomSystem/ReservationRemovalVerification.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ViesbucioPuslapis.Pages.RoomSystem
+{
+    public class ReservationRemovalVerificationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
