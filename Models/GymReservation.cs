e114282d3749da622c144789f6a25bbef2a7c0cb
AdamLeft <80246932+AdamLeft@users.noreply.github.com>
2023-12-09 13:42:43 +0200

    Added ability to reserve training sessions

--- Models/GymReservation.cs
+++ Models/GymReservation.cs
@@ -3,12 +3,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ViesbucioPuslapis.Models
 {
-    public class GymRezervation
+    public class GymReservation
     {
         [Key]
         public int id_Sporto_sales_rezervacija { get; set; }
-        public DateTime laiko_pradzia { get; set; }
-        public  DateTime laiko_pabaiga { get; set; }
+        public DateTime rezervacijos_laikas { get; set; }
         public int fk_Klientas_id_Naudotojas { get; set; }
         public int fk_Treniruote_treniruotes_nr { get; set; }
     }
